7e34dc2a8850061fbe08e0a1bef5fa3a6cf73560
Amer Mall <amer@icloud.com>
2021-12-15 23:02:21 -0500

    commit tree 78b8cfcc34ff45af7e3a201df98dbf285b43374f

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGift(gift) {
+//     console.log(`Wrapped ${gift} and added a bow!`);
+// }
+
+// wrapGift(gifts[0]);
+// wrapGift(gifts[1]);
+// wrapGift(gifts[2]);
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts)
+
+function writeCards(nameArray, eventName) {
+    const emptyArray = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        emptyArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    };
+
+    return emptyArray;
+}
+
+function countDown(posInteger) {
+    let i = posInteger;
+    while ( i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
